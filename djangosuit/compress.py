1c0ed5d359dc3d709cd99ab32ee53917e480190c
Kaspars Sprogis <darklow@gmail.com>
2013-03-08 01:23:31 +0200

    Removed unneeded debug code

--- djangosuit/compress.py
+++ djangosuit/compress.py
@@ -1,5 +1,4 @@
 from os.path import abspath
-from pprint import pprint
 from shutil import copyfile
 from djangosuit.settings import ENV_ROOT, PROJECT_ROOT
 
@@ -16,17 +15,7 @@ def post_less_compile(sender, **kwargs):
 
     compressed = context.get('compressed', None)
     if compressed and type == 'css':
-        pprint(compressed)
         css_file = abspath(ENV_ROOT + compressed['url'])
         target_file = abspath(
             PROJECT_ROOT + '/suit/suit/static/suit/css/suit.css')
         copyfile(css_file, target_file)
-
-        # pprint('ss')
-        # pprint(context.get('compressed', 'NOOO'))
-        # if isinstance(sender, CssCompressor):
-        # print(kwargs['sender'])
-        # print(kwargs['mode'])
-        # print(kwargs['type'])
-        # print(kwargs['context'])
-        #     print(kwargs)
